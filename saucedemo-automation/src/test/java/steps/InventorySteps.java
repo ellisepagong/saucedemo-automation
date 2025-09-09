97eadf465e9d0b3a2bb41e703a8ac464977a2643
ellisepagong <ellise.pagong@gmail.com>
2025-09-09 15:41:58 +0800

    Added steps for catalog verification

--- saucedemo-automation/src/test/java/steps/InventorySteps.java
+++ saucedemo-automation/src/test/java/steps/InventorySteps.java
@@ -1,17 +1,35 @@
 package steps;
 
-import io.cucumber.java.PendingException;
+import io.cucumber.java.DataTableType;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import models.CatalogItem;
 import org.junit.Assert;
 import pages.InventoryPage;
+import java.util.List;
+import java.util.Map;
 
 public class InventorySteps {
 
     private final InventoryPage page = new InventoryPage();
 
+    @DataTableType
+    public CatalogItem catalogItem(Map<String, String> row){
+            return new CatalogItem(row.get("Item Name"),
+                                    row.get("Item Description"),
+                                    row.get("Item Price"));
+        }
 
     @Then("I should be in inventory page")
     public void iShouldBeInInventoryPage() {
         Assert.assertTrue(page.verifyNavigation());
+    }
+
+    @And("I verify the following items in catalog")
+    public void iVerifyTheFollowingItemsInCatalog(List<CatalogItem> catalog) {
+        for(int i=0; i< catalog.size(); i++){
+            CatalogItem item = catalog.get(i);
+            Assert.assertTrue(item.equals(page.getCatalogItem(i)));
+        }
     }
 }
